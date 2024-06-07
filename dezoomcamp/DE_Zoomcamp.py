d718b52af4cf6de7a82cc6508458732bb31aa815
Mobile Desk <praise.obasi@trustscrin.com>
2024-06-07 11:56:47 +0100

    Update DE_Zoomcamp.py

--- dezoomcamp/DE_Zoomcamp.py
+++ dezoomcamp/DE_Zoomcamp.py
@@ -9,7 +9,7 @@ show_pages(
 
         # 2024 Content
         Section("DE Zoomcamp 2024", "🧙‍♂️"),
-        #Page("dezoomcamp/2024_cohort/Course_Overview.py", "Course Overview", "📚", in_section=True),
+        Page("dezoomcamp/2024_cohort/Course_Overview.py", "Course Overview", "📚", in_section=True),
         #Page("dezoomcamp/2024_cohort/Module_1_Introduction_&_Prerequisites.py", "Module 1 Introduction & Prerequisites", "1️⃣", in_section=True),
         #Page("dezoomcamp/2024_cohort/Module_2_Workflow_Orchestration.py", "Module 2 Workflow Orchestration", "2️⃣", in_section=True),
         #Page("dezoomcamp/2024_cohort/Workshop_1_Data_Ingestion.py", "Workshop 1 Data Ingestion", "🛠️", in_section=True),
